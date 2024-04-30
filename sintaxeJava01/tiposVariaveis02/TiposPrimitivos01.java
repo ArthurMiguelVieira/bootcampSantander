59d386f193aefad169d1be9b855b174ae90c41a8
Arthur Miguel <arthur.migue.dev@gmail.com>
2024-04-29 21:19:29 -0300

    Anotações tipos e variaveis java

---
+++ sintaxeJava01/tiposVariaveis02/TiposPrimitivos01.java
@@ -0,0 +1,37 @@
+public class TiposPrimitivos01 {
+    public static void main(String[] args) {
+        // No Java existem 8 tipos primitivos
+        //Uma variável é um espaço de memória nomeado que armazena um valor. 
+        // Exemplo de uso de int
+        int numero = 10; // Variável do tipo int para armazenar números inteiros
+        System.out.println("Valor de numero: " + numero);
+
+        // Exemplo de uso de byte
+        byte byteValor = 100; // Variável do tipo byte para armazenar números inteiros pequenos
+        System.out.println("Valor de byteValor: " + byteValor);
+
+        // Exemplo de uso de short
+        short shortValor = 1000; // Variável do tipo short para armazenar números inteiros um pouco maiores
+        System.out.println("Valor de shortValor: " + shortValor);
+
+        // Exemplo de uso de long
+        long longValor = 1000000000L; // Variável do tipo long para armazenar números inteiros longos. O sufixo 'L' indica que é um literal long
+        System.out.println("Valor de longValor: " + longValor);
+
+        // Exemplo de uso de double
+        double doubleValor = 3.14; // Variável do tipo double para armazenar números de ponto flutuante de dupla precisão. Mais preciso que float
+        System.out.println("Valor de doubleValor: " + doubleValor);
+
+        // Exemplo de uso de float
+        float floatValor = 3.14f; // Variável do tipo float para armazenar números de ponto flutuante de precisão simples. O sufixo 'f' indica que é um literal float
+        System.out.println("Valor de floatValor: " + floatValor);
+
+        // Exemplo de uso de boolean
+        boolean booleanValor = true; // Variável do tipo boolean para armazenar valores lógicos (true ou false)
+        System.out.println("Valor de booleanValor: " + booleanValor);
+
+        // Exemplo de uso de char
+        char charValor = 'A'; // Variável do tipo char para armazenar caracteres únicos
+        System.out.println("Valor de charValor: " + charValor);
+    }
+}
---
+++ sintaxeJava01/tiposVariaveis02/variaveisConstantes02.java
@@ -0,0 +1,10 @@
+public class variaveisConstantes02 {
+    public static void main(String[] args) {
+        //quando queremos tornar uma variavel imutavel colamos final em seu inicio
+
+        final double PI_VALUE = 3.14;
+
+        System.out.println(PI_VALUE);
+        
+    }    
+}
